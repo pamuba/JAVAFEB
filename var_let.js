084ee2133646e1a458ed5bba2d81ccfa1b58878d
pamuba <partha.bora24@gmail.com>
2021-03-10 00:03:52 +0530

    13

--- var_let.js
+++ var_let.js
@@ -1,17 +1,28 @@
 //Scoping
-function run(){
-    var foo = "Foo"
-    let bar = "Bar"
-    console.log(foo, bar)
+// function run(){
+//     var foo = "Foo"
+//     let bar = "Bar"
+//     console.log(foo, bar)
 
-    {
-        var moo = "Moo"
-        let baz = "Baz"
-        console.log(moo, baz)
-    }
-    console.log(moo)
-    // console.log(baz)
+//     {
+//         var moo = "Moo"
+//         let baz = "Baz"
+//         console.log(moo, baz)
+//     }
+//     console.log(moo)
+//     // console.log(baz)
+// }
+// console.log(foo)
+// run();
+
+var funcs = [];
+
+for (let i = 0; i < 3; i++) {
+    funcs[i] = function(){
+        console.log("My Value: "+ i)
+    }   
 }
-console.log(foo)
-run();
 
+for (var j = 0; j < 3; j++) {
+    funcs[j]();
+}
