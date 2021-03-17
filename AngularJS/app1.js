ca3e8ab144fbf3a060abde9f56a432563227ec46
pamuba <partha.bora24@gmail.com>
2021-03-17 23:04:27 +0530

    71

---
+++ AngularJS/app1.js
@@ -0,0 +1,16 @@
+var myApp = angular.module("myModule",[]);
+
+myApp.controller("myController", function($scope){
+    $scope.message = "Hello World"
+})
+
+myApp.controller("myController1", function($scope){
+    $scope.message = "Hello World from 2nd Controller"
+    var employee = {
+        firstName:"John",
+        lastName:"Smith",
+        gender:"Male"
+    }
+    //attach the ob to the scope
+    $scope.employee = employee
+})
