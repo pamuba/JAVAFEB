6c9c8f7587eab3be2f8b10a4a98e3e887db33d9a
pamuba <partha.bora24@gmail.com>
2021-03-23 23:14:55 +0530

    112

--- MEANJS/public/controllers/controller.js
+++ MEANJS/public/controllers/controller.js
@@ -22,4 +22,16 @@ function AppCtrl($scope, $http){  //DI
             refresh();
         })
     }
+    $scope.edit = function(id){
+        console.log(id)
+        $http.get('/contactlist/'+id).success(function(response){
+            $scope.contact = response;
+        })
+    }
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        $http.put('/contactlist/'+ $scope.contact._id, $scope.contact).success(function(response){
+            refresh();
+        })
+    }
 }
--- MEANJS/server.js
+++ MEANJS/server.js
@@ -38,6 +38,25 @@ app.delete('/contactlist/:id', function(req, res){
         }
         res.json(doc);
     })
+});
+
+app.get('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id)
+    db.contactlist.findOne({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc)
+    })
+})
+
+app.put('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.contactlist.findAndModify({query: {_id:mongojs.ObjectId(id)},
+        update:{$set:{name:req.body.name, email:req.body.email, number:req.body.number}},
+        new:true}, function(err, doc){
+            res.json(doc);
+        }
+    )
 })
 
 app.listen(3000);
